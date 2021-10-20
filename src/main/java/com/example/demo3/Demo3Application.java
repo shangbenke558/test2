74e33ad90d5a7960e0df5ec8efb65151932d0065
benkeshang <benke.shang@advantech.com.cn>
2021-10-20 14:50:41 +0800

    lll

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Demo3Application {
 
+//hhhhh
     public static void main(String[] args) {
         SpringApplication.run(Demo3Application.class, args);
     }
